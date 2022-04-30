22ecc6cc85314f88fa2ee509fe5a5522a59272fa
TanbirRamim <Tanbirramim420@gmail.com>
2022-04-30 20:05:19 +0600

    server connected with mongodb

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,10 +9,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zug5c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 app.get("/", (req, res) => {
   res.send("Running my Wholesome server");
 });
 
 app.listen(port, () => {
-  console.log("Wholesome tech is running");
+  console.log("Wholesome tech is running", port);
 });
