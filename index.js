3735da962b1ffd69e2e32ca8e3c6d721271b9941
TanbirRamim <Tanbirramim420@gmail.com>
2022-05-04 04:42:01 +0600

    card detail added

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
-//middleware
 app.use(cors());
 app.use(express.json());
 
@@ -26,6 +25,13 @@ async function run() {
       const cursor = productCollection.find(query);
       const products = await cursor.toArray();
       res.send(products);
+    });
+    app.get("/cardDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const item = await productCollection.findOne(query);
+
+      res.send({ item });
     });
   } finally {
   }
